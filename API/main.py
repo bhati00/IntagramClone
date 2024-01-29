60ffed707aa4418c5c738e991bc0967f569de3a8
prince <137474916+princeThynk@users.noreply.github.com>
2024-01-30 00:57:02 +0530

    googleauth

--- API/main.py
+++ API/main.py
@@ -3,11 +3,13 @@ from data.database import engine
 from routers import users,posts
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from auth import googleAuth
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(authentication.router)
+app.include_router(googleAuth.router)
 app.mount("/images", StaticFiles(directory='images'), name = 'images')
 
 
